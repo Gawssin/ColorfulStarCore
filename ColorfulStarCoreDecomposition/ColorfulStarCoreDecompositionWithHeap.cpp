9560b44502f2e8760a9768301f2a357b738557ba
Gawssin <31264472+Gawssin@users.noreply.github.com>
2021-11-18 14:14:27 +0800

    add basic version

--- ColorfulStarCoreDecomposition/ColorfulStarCoreDecompositionWithHeap.cpp
+++ ColorfulStarCoreDecomposition/ColorfulStarCoreDecompositionWithHeap.cpp
@@ -16,88 +16,43 @@
 #include "../header/ColorfulStarCore.hpp"
 using namespace std;
 
-#define debug 0
 
 int main(int argc, char** argv)
 {
-
-	char* argv1, * argv2;
+	char* argv1, * argv2, * argv3;
 	argv1 = argv[1], argv2 = argv[2];
-
-
-	//--------- readCMD begin
-
-	//readCMD rCMD(argc, argv);
-	//argv1 = rCMD.read();
-	//argv2 = rCMD.read();
-
-	//--------- readCMD end
+	int basicFlag = 0;
+	if (argc >= 4 && strcmp(argv[3], "basic") == 0)
+	{
+		basicFlag = 1;
+	}
+	cout << "Version: " << ( basicFlag ? "Basic (HStarDP)":"Advanced (HStarCD)" ) << endl << endl;
 	
-
 	auto t0 = getTime();
 
 	Graph g;
 	int h = atoi(argv1);
-	cout << "Reading edgelist from file " << argv2 << endl;
+	cout << "Reading edgelist from file: " << argv2 << endl;
 	g.readedgelist(argv2);
 	cout << "Reading edgelist finished!" << endl;
 	g.mkGraph();
 	cout << "mkGraph finished!" << endl;
 	auto t1 = getTime();
 
-	//long long tol;
-	//long long * cnt = new long long[g.n];
-	//g.kClique(k, &tol, cnt);
-
-	//printf("%d-clique: %lld\n", k, tol);
-
-	//return 0;
-
 	int* color = new int[g.n];
 	int colorNum = g.color(color);
-	printf("colorNum = %d\n", colorNum);
-
-	if (debug)
-	{
-		//int setColor[] = { 0,3,0,1,2,0,4,3,1,0 };
-		//color = setColor;
-		for (int i = 0; i < g.n; i++)
-		{
-			printf("id = %d color = %d\n", i, color[i]);
-		}
-	}
+	printf("Total number of colors: %d\n", colorNum);
 
 	double** dp = new double* [g.n];
 	int** CC = new int* [g.n];
-	initColStarDegree(g, dp, h, colorNum, color, CC);
+	double* ColofulStarCoreNum = new double[g.n];
+
+	initColStarDegree(g, dp, h, colorNum, color, CC, basicFlag);
+	ColorfulStarCoreDecomp(g, dp, h, color, CC, ColofulStarCoreNum, colorNum, basicFlag);
+
 
 	auto t2 = getTime();
-	
-
-	if (debug)
-	{
-		for (int i = 0; i < g.n; i++)
-		{
-			printf("id = %d starDegree = %lf\n", i, dp[i][h - 1]);
-		}
-	}
-
-	double* ColofulStarCoreNum = new double[g.n];
-	ColorfulStarCoreDecomp(g, dp, h, color, CC, ColofulStarCoreNum);
-
-	if (debug)
-	{
-		for (int i = 0; i < g.n; i++)
-		{
-			printf("id = %d starDegree = %lf\n", i, ColofulStarCoreNum[i]);
-		}
-	}
-
-	auto t3 = getTime();
-	printf("- Overall time = %lfs\n", ((double)timeGap(t1, t3)) / 1e6);
-
-	printf("The End\n");
+	printf("- Overall time = %lfs\n", ((double)timeGap(t1, t2)) / 1e6);
 
 	return 0;
-
 }
--- header/ColorfulStarCore.hpp
+++ header/ColorfulStarCore.hpp
@@ -1,5 +1,5 @@
 
-void initColStarDegree(Graph& g, double** dp, int h, int colorNum, int* color, int** CC)
+void initColStarDegree(Graph& g, double** dp, int h, int colorNum, int* color, int** CC, int basicFlag)
 {
 	double* NotColor0 = new double[h]();
 	double* MustColor0 = new double[h]();
@@ -18,26 +18,169 @@ void initColStarDegree(Graph& g, double** dp, int h, int colorNum, int* color, i
 
 		//int* tol = new int[k + 1];
 
-		NotColor0[0] = 1;
-		for (int c = 1; c <= colorNum_i; c++)
+		if (basicFlag == 0)
 		{
-			for (int j = h - 1; j > 0; j--)
-				NotColor0[j] = NotColor0[j - 1] * CC[i][c] + NotColor0[j];
-		}
+			NotColor0[0] = 1;
+			for (int c = 1; c <= colorNum_i; c++)
+			{
+				for (int j = h - 1; j > 0; j--)
+					NotColor0[j] = NotColor0[j - 1] * CC[i][c] + NotColor0[j];
+			}
 
-		for (int j = 1; j < h; j++)
-		{
-			MustColor0[j] = NotColor0[j - 1] * CC[i][0];
-			dp[i][j] = MustColor0[j] + NotColor0[j];
+			for (int j = 1; j < h; j++)
+			{
+				MustColor0[j] = NotColor0[j - 1] * CC[i][0];
+				dp[i][j] = MustColor0[j] + NotColor0[j];
+				NotColor0[j - 1] = 0;
+			}
+			NotColor0[h - 1] = 0;
 		}
-		fill(NotColor0, NotColor0 + h, 0.0);
-		fill(MustColor0, MustColor0 + h, 0.0);
+		else
+		{
+			dp[i][0] = 1;
+			for (int c = 0; c <= colorNum_i; c++)
+			{
+				for (int j = h - 1; j > 0; j--)
+					dp[i][j] = dp[i][j] + dp[i][j - 1] * CC[i][c];
+			}
+		}
+		
+
+
+		//fill(NotColor0, NotColor0 + h, 0.0);
+		//fill(MustColor0, MustColor0 + h, 0.0);
 	}
 	delete[] NotColor0;
 	delete[] MustColor0;
 }
 
-void ColorfulStarCoreDecomp(Graph& g, double** dp, int h, int* color, int** CC, double* ColofulStarCoreNum, double *maxCore = 0, int * maxCoreNum = 0)
+void ColorfulStarCoreDecomp(Graph& g, double** dp, int h, int* color, int** CC, double* ColofulStarCoreNum, int colorNum, int basicFlag, double *maxCore = 0, int * maxCoreNum = 0)
+{
+	double* tmpDP = new double[g.n];
+	for (int i = 0; i < g.n; i++) tmpDP[i] = dp[i][h - 1];
+	bheapLLU<double>* heap = mkheapLLU<double>(g.n, tmpDP);
+
+	double maxStarDegree = -1;
+	for (int i = 0; i < g.n; i++)
+	{
+		maxStarDegree = max(maxStarDegree, dp[i][h - 1]);
+	}
+	printf("The maximum colorful h-star degree: %lf\n", maxStarDegree);
+
+	int leftN = g.n, leftM = g.e;
+
+	double* NotColor = new double[h]();
+	double* MustColor = new double[h]();
+
+	double starCoreNum = 0;
+	int times = 0, maxN = 0, maxM = 0;
+	keyvalueLLU<double> kv;
+
+	while (leftN > 0)
+	{
+		times++;
+		kv = popminLLU<double>(heap);
+
+		if (kv.value > starCoreNum)
+		{
+			starCoreNum = kv.value;
+			maxN = leftN;
+			maxM = leftM;
+		}
+
+
+		if (times % 100000 == 0)
+			printf("times = %d\tleft nodes = %-10d\ttolMax = %lf\tmaxN = %d\tmaxM = %d\tdensity = %lf\n", times, leftN, starCoreNum, maxN, maxM, maxN?(1.0 * maxM / maxN) : 0.0);
+
+		leftN--;
+		int i = kv.key;
+		leftM -= g.deg[i];
+		ColofulStarCoreNum[i] = starCoreNum;
+
+		for (int j = g.cd[i]; j < g.cd[i] + g.deg[i]; j++)
+		{
+			int nbr = g.adj[j];
+
+			for (int p = g.cd[nbr]; p < g.cd[nbr] + g.deg[nbr]; p++)
+			{
+				int hnbr = g.adj[p];
+				if (hnbr == i)
+				{
+					swap(g.adj[p], g.adj[g.cd[nbr] + g.deg[nbr] - 1]);
+					g.deg[nbr]--;
+					break;
+				}
+			}
+
+			CC[nbr][color[i]]--;
+
+			NotColor[0] = 1;
+			if (basicFlag == 0)
+			{
+				for (int p = 1; p < h; p++)
+				{
+					MustColor[p] = NotColor[p - 1] * (CC[nbr][color[i]] + 1);
+					NotColor[p] = dp[nbr][p] - MustColor[p];
+				}
+				for (int p = 1; p < h; p++)
+				{
+					MustColor[p] = NotColor[p - 1] * CC[nbr][color[i]];
+					dp[nbr][p] = NotColor[p] + MustColor[p];
+				}
+			}
+			else
+			{
+				fill(dp[nbr] + 1, dp[nbr] + h, 0);
+				for (int c = 0; c < colorNum; c++)
+				{
+					for (int j = h - 1; j > 0; j--)
+						dp[nbr][j] = dp[nbr][j] + dp[nbr][j - 1] * CC[nbr][c];
+				}
+			}
+
+			updateLLU(heap, nbr, dp[nbr][h - 1]);
+		}
+		g.deg[i] = 0;
+	}
+
+	if(maxCore != 0) *maxCore = starCoreNum;
+	if(maxCoreNum != 0) *maxCoreNum = maxN;
+	/////
+	printf("End:\ntimes = %d\tleft nodes = %-10d\ttolMax = %lf\tmaxN = %d\tmaxM = %d\tdensity = %lf\n", times, leftN, starCoreNum, maxN, maxM, 1.0 * maxM / maxN);
+
+	delete[] NotColor;
+	delete[] MustColor;
+}
+
+
+void initColStarDegreeBasic(Graph& g, double** dp, int h, int colorNum, int* color, int** CC)
+{
+	for (int i = 0; i < g.n; i++)
+	{
+		dp[i] = new double[h]();
+		int colorNum_i = 0;
+		//int* C = new int[colorNum]();
+		CC[i] = new int[colorNum]();
+		for (int j = g.cd[i]; j < g.cd[i] + g.deg[i]; j++)
+		{
+			int nbr = g.adj[j];
+			CC[i][color[nbr]]++;
+			colorNum_i = max(colorNum_i, color[nbr]);
+		}
+
+		//int* tol = new int[k + 1];
+
+		//NotColor0[0] = 1;
+		dp[i][0] = 1;
+		for (int c = 0; c <= colorNum_i; c++)
+		{
+			for (int j = h - 1; j > 0; j--)
+				dp[i][j] = dp[i][j] + dp[i][j-1] * CC[i][c];
+		}
+	}
+}
+
+void ColorfulStarCoreDecompBasic(Graph& g, double** dp, int h, int* color, int** CC, double* ColofulStarCoreNum, int colorNum, double* maxCore = 0, int* maxCoreNum = 0)
 {
 	double* tmpDP = new double[g.n];
 	for (int i = 0; i < g.n; i++) tmpDP[i] = dp[i][h - 1];
@@ -52,8 +195,8 @@ void ColorfulStarCoreDecomp(Graph& g, double** dp, int h, int* color, int** CC, 
 
 	int leftN = g.n, leftM = g.e;
 
-	double* NotColor = new double[h]();
-	double* MustColor = new double[h]();
+	//double* NotColor = new double[h]();
+	//double* MustColor = new double[h]();
 
 	double starCoreNum = 0;
 	int times = 0, maxN = 0, maxM = 0;
@@ -75,7 +218,7 @@ void ColorfulStarCoreDecomp(Graph& g, double** dp, int h, int* color, int** CC, 
 
 
 		if (times % 50000 == 0)
-			printf("times = %d left nodes = %d tolMax = %lf maxN = %d maxM = %d density = %lf\n", times, leftN, starCoreNum, maxN, maxM, maxN?(1.0 * maxM / maxN) : 0.0);
+			printf("times = %d left nodes = %d tolMax = %lf maxN = %d maxM = %d density = %lf\n", times, leftN, starCoreNum, maxN, maxM, maxN ? (1.0 * maxM / maxN) : 0.0);
 
 		//if (times == g.n - 2)
 		//	printf("kv.value = %lf times = %d leftN = %d, leftM = %d tolMax = %lf maxN = %d maxM = %d density = %lf\n", kv.value, times, leftN, leftM, starCoreNum, maxN, maxM, 1.0 * maxM / maxN);
@@ -104,30 +247,22 @@ void ColorfulStarCoreDecomp(Graph& g, double** dp, int h, int* color, int** CC, 
 
 			CC[nbr][color[i]]--;
 
-			NotColor[0] = 1;
-			for (int p = 1; p < h; p++)
-			{
-				MustColor[p] = NotColor[p - 1] * (CC[nbr][color[i]] + 1);
-				NotColor[p] = dp[nbr][p] - MustColor[p];
-			}
+			fill(dp[nbr] + 1, dp[nbr] + h, 0);
 
-			for (int p = 1; p < h; p++)
+			for (int c = 0; c < colorNum; c++)
 			{
-				MustColor[p] = NotColor[p - 1] * CC[nbr][color[i]];
-				dp[nbr][p] = NotColor[p] + MustColor[p];
+				for (int j = h - 1; j > 0; j--)
+					dp[nbr][j] = dp[nbr][j] + dp[nbr][j - 1] * CC[nbr][c];
 			}
 			updateLLU(heap, nbr, dp[nbr][h - 1]);
 		}
 		g.deg[i] = 0;
 	}
 
-	if(maxCore != 0) *maxCore = starCoreNum;
-	if(maxCoreNum != 0) *maxCoreNum = maxN;
+	if (maxCore != 0)* maxCore = starCoreNum;
+	if (maxCoreNum != 0)* maxCoreNum = maxN;
 	/////
 	printf("End: times = %d left nodes = %d tolMax = %lf maxN = %d maxM = %d density = %lf\n", times, leftN, starCoreNum, maxN, maxM, 1.0 * maxM / maxN);
-
-	delete[] NotColor;
-	delete[] MustColor;
 }
 
 
